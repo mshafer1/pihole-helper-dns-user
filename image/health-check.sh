20a45a5670c2d57fd6f0d5a21621f010f1cd2fd8
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2026-08-01 18:18:17 -0500

    get health-check working

--- image/health-check.sh
+++ image/health-check.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -sSf http://localhost:${SERVER_PORT:-5000}/api/health > /dev/null 2>&1 || {
+curl -sSf http://localhost:5000/api/health > /dev/null 2>&1 || {
     echo "Pi-hole API is not healthy. Please check the Pi-hole container."
     exit 1
 }
